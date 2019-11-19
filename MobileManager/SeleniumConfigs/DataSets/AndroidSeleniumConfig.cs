a01944e4486643ae6069e7de46178109d0a1f914
Martin Kroupa <martin.kroupa@dixonscarphone.com>
2019-11-19 15:48:33 +0100

    Fixed docs

--- MobileManager/SeleniumConfigs/DataSets/AndroidSeleniumConfig.cs
+++ MobileManager/SeleniumConfigs/DataSets/AndroidSeleniumConfig.cs
@@ -28,35 +28,35 @@ namespace MobileManager.SeleniumConfigs.DataSets
 
         /// <inheritdoc />
         /// <summary>
-        /// Gets or sets the Id.
+        /// Gets the Id.
         /// </summary>
         /// <value>The Id.</value>
         public string Id { get; }
 
         /// <inheritdoc />
         /// <summary>
-        /// Gets or sets the Name.
+        /// Gets the Name.
         /// </summary>
         /// <value>The Name.</value>
         public string Name { get; }
 
         /// <inheritdoc />
         /// <summary>
-        /// Gets or sets the AppiumEndpoint.
+        /// Gets the AppiumEndpoint.
         /// </summary>
         /// <value>The AppiumEndpoint.</value>
         public string AppiumEndpoint { get; }
 
         /// <inheritdoc />
         /// <summary>
-        /// Gets or sets the Host.
+        /// Gets the Host.
         /// </summary>
         /// <value>The Host.</value>
         public string Host { get; }
 
         /// <inheritdoc />
         /// <summary>
-        /// Gets or sets the Port.
+        /// Gets the Port.
         /// </summary>
         /// <value>The Port.</value>
         public string Port { get; }
--- MobileManager/SeleniumConfigs/DataSets/Interfaces/ISeleniumConfig.cs
+++ MobileManager/SeleniumConfigs/DataSets/Interfaces/ISeleniumConfig.cs
@@ -6,31 +6,31 @@
     public interface ISeleniumConfig
     {
         /// <summary>
-        /// Gets or sets the identifier.
+        /// Gets the identifier.
         /// </summary>
         /// <value>The identifier.</value>
         string Id { get; }
 
         /// <summary>
-        /// Gets or sets the name.
+        /// Gets the name.
         /// </summary>
         /// <value>The name.</value>
         string Name { get; }
         
         /// <summary>
-        /// Gets or sets the appium endpoint.
+        /// Gets the appium endpoint.
         /// </summary>
         /// <value>The appium endpoint.</value>
         string AppiumEndpoint { get; }
 
         /// <summary>
-        /// Gets or sets Host.
+        /// Gets Host.
         /// </summary>
         /// <value>The Host.</value>
         string Host { get; }
 
         /// <summary>
-        /// Gets or sets Port.
+        /// Gets Port.
         /// </summary>
         /// <value>The Port.</value>
         string Port { get; }
